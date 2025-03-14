6bd2e99e5c391a54119ce19cfecec651eef78705
Jose Cazorla <40724062+JCazorla90@users.noreply.github.com>
2025-03-14 10:32:44 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 // Partículas espaciales
 const canvas = document.getElementById('particles');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -29,13 +28,13 @@ class Particle {
   }
 }
 
-function init() {
+function initParticles() {
   for (let i = 0; i < numberOfParticles; i++) {
     particlesArray.push(new Particle());
   }
 }
 
-function animate() {
+function animateParticles() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
@@ -46,13 +45,170 @@ function animate() {
       particlesArray.push(new Particle());
     }
   }
-  requestAnimationFrame(animate);
+  requestAnimationFrame(animateParticles);
 }
 
+// D3.js Force Simulation para Tech
+const techSvg = d3.select('#tech-svg');
+const techWidth = techSvg.node().getBoundingClientRect().width;
+const techHeight = 300;
+
+const techData = [
+  { name: 'AWS', url: 'https://aws.amazon.com', img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/amazonwebservices/amazonwebservices-original-wordmark.svg' },
+  { name: 'Azure', url: 'https://azure.microsoft.com', img: 'https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg' },
+  { name: 'GCP', url: 'https://cloud.google.com', img: 'https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg' },
+  { name: 'Kubernetes', url: 'https://kubernetes.io', img: 'https://www.vectorlogo.zone/logos/kubernetes/kubernetes-icon.svg' },
+  { name: 'Docker', url: 'https://www.docker.com', img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg' },
+  { name: 'Python', url: 'https://www.python.org', img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg' },
+  { name: 'Git', url: 'https://git-scm.com', img: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg' },
+  { name: 'Grafana', url: 'https://grafana.com', img: 'https://www.vectorlogo.zone/logos/grafana/grafana-icon.svg' },
+  { name: 'Jenkins', url: 'https://www.jenkins.io', img: 'https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg' },
+  { name: 'Linux', url: 'https://www.linux.org', img: 'https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg' },
+  { name: 'TensorFlow', url: 'https://www.tensorflow.org', img: 'https://www.vectorlogo.zone/logos/tensorflow/tensorflow-icon.svg' },
+  { name: 'RabbitMQ', url: 'https://www.rabbitmq.com', img: 'https://www.vectorlogo.zone/logos/rabbitmq/rabbitmq-icon.svg' }
+];
+
+const techSimulation = d3.forceSimulation(techData)
+  .force('charge', d3.forceManyBody().strength(-50))
+  .force('center', d3.forceCenter(techWidth / 2, techHeight / 2))
+  .force('collision', d3.forceCollide().radius(40))
+  .on('tick', techTicked);
+
+const techNodes = techSvg.selectAll('g')
+  .data(techData)
+  .enter()
+  .append('g')
+  .call(d3.drag()
+    .on('start', dragStarted)
+    .on('drag', dragged)
+    .on('end', dragEnded));
+
+techNodes.append('image')
+  .attr('xlink:href', d => d.img)
+  .attr('width', 40)
+  .attr('height', 40)
+  .attr('x', -20)
+  .attr('y', -20);
+
+techNodes.append('text')
+  .attr('dy', '30')
+  .attr('text-anchor', 'middle')
+  .style('fill', '#e0e0e0')
+  .style('font-size', '12px')
+  .text(d => d.name);
+
+techSvg.on('mousemove', function(event) {
+  const [mx, my] = d3.pointer(event);
+  techData.forEach(d => {
+    const dx = d.x - mx;
+    const dy = d.y - my;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+    if (dist < 100) {
+      d.vx += dx / dist * 5;
+      d.vy += dy / dist * 5;
+    }
+  });
+});
+
+function techTicked() {
+  techNodes.attr('transform', d => `translate(${d.x},${d.y})`);
+}
+
+// D3.js Force Simulation para Connect
+const connectSvg = d3.select('#connect-svg');
+const connectWidth = connectSvg.node().getBoundingClientRect().width;
+const connectHeight = 200;
+
+const connectData = [
+  { name: 'Twitter', url: 'https://twitter.com/jcazorla90', img: 'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg' },
+  { name: 'LinkedIn', url: 'https://linkedin.com/in/jose-cazorla-gijón', img: 'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg' },
+  { name: 'Dev.to', url: 'https://dev.to/jcazorla90', img: 'https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/dev-dot-to.svg' },
+  { name: 'CodePen', url: 'https://codepen.io/jcazorla90', img: 'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/codepen.svg' },
+  { name: 'Stack Overflow', url: 'https://stackoverflow.com/users/tipodeincognito', img: 'https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/stack-overflow.svg' }
+];
+
+const connectSimulation = d3.forceSimulation(connectData)
+  .force('charge', d3.forceManyBody().strength(-50))
+  .force('center', d3.forceCenter(connectWidth / 2, connectHeight / 2))
+  .force('collision', d3.forceCollide().radius(40))
+  .on('tick', connectTicked);
+
+const connectNodes = connectSvg.selectAll('g')
+  .data(connectData)
+  .enter()
+  .append('g')
+  .call(d3.drag()
+    .on('start', dragStarted)
+    .on('drag', dragged)
+    .on('end', dragEnded));
+
+connectNodes.append('a')
+  .attr('xlink:href', d => d.url)
+  .attr('target', '_blank')
+  .append('image')
+  .attr('xlink:href', d => d.img)
+  .attr('width', 40)
+  .attr('height', 40)
+  .attr('x', -20)
+  .attr('y', -20);
+
+connectNodes.append('text')
+  .attr('dy', '30')
+  .attr('text-anchor', 'middle')
+  .style('fill', '#e0e0e0')
+  .style('font-size', '12px')
+  .text(d => d.name);
+
+connectSvg.on('mousemove', function(event) {
+  const [mx, my] = d3.pointer(event);
+  connectData.forEach(d => {
+    const dx = d.x - mx;
+    const dy = d.y - my;
+    const dist = Math.sqrt(dx * dx + dy * dy);
+    if (dist < 100) {
+      d.vx += dx / dist * 5;
+      d.vy += dy / dist * 5;
+    }
+  });
+});
+
+function connectTicked() {
+  connectNodes.attr('transform', d => `translate(${d.x},${d.y})`);
+}
+
+// Funciones de arrastre
+function dragStarted(event, d) {
+  if (!event.active) {
+    techSimulation.alphaTarget(0.3).restart();
+    connectSimulation.alphaTarget(0.3).restart();
+  }
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(event, d) {
+  d.fx = event.x;
+  d.fy = event.y;
+}
+
+function dragEnded(event, d) {
+  if (!event.active) {
+    techSimulation.alphaTarget(0);
+    connectSimulation.alphaTarget(0);
+  }
+  d.fx = null;
+  d.fy = null;
+}
+
+// Inicializar y animar
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  techWidth = techSvg.node().getBoundingClientRect().width;
+  connectWidth = connectSvg.node().getBoundingClientRect().width;
+  techSimulation.force('center', d3.forceCenter(techWidth / 2, techHeight / 2));
+  connectSimulation.force('center', d3.forceCenter(connectWidth / 2, connectHeight / 2));
 });
 
-init();
-animate();
+initParticles();
+animateParticles();
